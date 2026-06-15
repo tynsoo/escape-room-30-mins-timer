16e43f55e531594c5cf9eef674c6e766588768ec
tynsoo <christeensolis2209@gmail.com>
2026-06-15 17:17:00 +0800

    revised the foundation week escape room timer to escape room timer

--- server.js
+++ server.js
@@ -70,6 +70,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
